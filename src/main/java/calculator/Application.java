a047690b4a3109590943fd8869533eff0e204765
sihyun10 <hyhy200170@gmail.com>
2025-10-15 18:16:07 +0900

    feat(extractAndSplit): 문자열 덧셈 계산기의 핵심 기능(구분자 판단 및 문자열 분리) 구현
    입력 문자열에서 커스텀 구분자와 기본 구분자를 판단하여
    커스텀 구분자이면 유효성 검증(비어있거나 줄바꿈 문자가 없거나 하나의 문자만 허용) 처리해주고,
    추출된 구분자를 기준으로 숫자 문자열을 분리하는 로직을 구현하였다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,60 @@
 package calculator;
 
+import static java.util.regex.Pattern.*;
+
+import calculator.exception.InvalidInputException;
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+        System.out.println(Arrays.toString(extractAndSplit(input)));
+    }
+
+    private static final String CUSTOM_PREFIX = "//";
+    private static final String NEWLINE = "\n";
+    private static final String DEFAULT_DELIMITER_REGEX = ",|:";
+
+    public static String[] extractAndSplit(String input) {
+        if (input == null || input.isBlank()) {
+            throw new InvalidInputException("[ERROR] 입력값이 비어있습니다.");
+        }
+
+        input = input.replace("\\n", "\n");
+
+        String delimiter;
+        String numbersPart;
+
+        if (input.startsWith(CUSTOM_PREFIX)) {
+            int nlIdx = input.indexOf(NEWLINE);
+            if (nlIdx == -1) {
+                throw new InvalidInputException("[ERROR] 커스텀 구분자 선언 후 줄바꿈 문자가 없습니다.");
+            }
+
+            String customDelimiter = input.substring(2, nlIdx);
+            if (customDelimiter.isEmpty()) {
+                throw new InvalidInputException("[ERROR] 커스텀 구분자가 비어 있습니다.");
+            }
+
+            // 커스텀 구분자는 1글자만 허용한다
+            if (customDelimiter.length() > 1) {
+                throw new InvalidInputException("[ERROR] 커스텀 구분자는 하나의 문자만 입력할 수 있다.");
+            }
+
+            numbersPart = input.substring(nlIdx + 1);
+            if (numbersPart.isEmpty()) {
+                throw new InvalidInputException("[ERROR] 커스텀 구분자 지정 후 숫자가 입력되지 않았습니다.");
+            }
+
+            // 특수문자 처리
+            delimiter = quote(customDelimiter);
+        } else {
+            delimiter = DEFAULT_DELIMITER_REGEX;
+            numbersPart = input;
+        }
+
+        return numbersPart.split(delimiter);
     }
 }
---
+++ src/main/java/calculator/exception/InvalidInputException.java
@@ -0,0 +1,8 @@
+package calculator.exception;
+
+public class InvalidInputException extends IllegalArgumentException {
+
+    public InvalidInputException(String message) {
+        super(message);
+    }
+}
