dd630d91e83b7e2d8b5fdd5ce2efa5358fef669d
sihyun10 <hyhy200170@gmail.com>
2025-10-17 22:36:40 +0900

    test: assertThatThrownBy()로 수정
    보통 AssertJ 체이닝이 가독성이 좋고, 복합 검증이 가능하기에
    assertThrows()보다 assertThatThrownBy()를 쓰는게 더 나을거같아 수정하였다.

--- src/test/java/calculator/util/DelimiterExtractorTest.java
+++ src/test/java/calculator/util/DelimiterExtractorTest.java
@@ -1,8 +1,9 @@
 package calculator.util;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import calculator.exception.ErrorMessage;
 import calculator.exception.InvalidInputException;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -11,45 +12,40 @@ class DelimiterExtractorTest {
     @Test
     @DisplayName("입력값이 null이면 예외 발생")
     void 입력_null_예외() {
-        Assertions.assertThrows(
-                InvalidInputException.class,
-                () -> DelimiterExtractor.extract(null), ErrorMessage.EMPTY_INPUT
-        );
+        assertThatThrownBy(() -> DelimiterExtractor.extract(null))
+                .isInstanceOf(InvalidInputException.class)
+                .hasMessage(ErrorMessage.EMPTY_INPUT);
     }
 
     @Test
     @DisplayName("//로 시작했지만 줄바꿈이 없으면 예외 발생")
     void 줄바꿈_없음_예외() {
-        Assertions.assertThrows(
-                InvalidInputException.class,
-                () -> DelimiterExtractor.extract("//;1;2;3"), ErrorMessage.NO_NEWLINE_AFTER_CUSTOM
-        );
+        assertThatThrownBy(() -> DelimiterExtractor.extract("//;1;2;3"))
+                .isInstanceOf(InvalidInputException.class)
+                .hasMessage(ErrorMessage.NO_NEWLINE_AFTER_CUSTOM);
     }
 
     @Test
     @DisplayName("커스텀 구분자가 비어있으면 예외 발생")
     void 커스텀구분자_비어있음_예외() {
-        Assertions.assertThrows(
-                InvalidInputException.class,
-                () -> DelimiterExtractor.extract("//\n1;2;3"), ErrorMessage.EMPTY_CUSTOM_DELIMITER
-        );
+        assertThatThrownBy(() -> DelimiterExtractor.extract("//\n1;2;3"))
+                .isInstanceOf(InvalidInputException.class)
+                .hasMessage(ErrorMessage.EMPTY_CUSTOM_DELIMITER);
     }
 
     @Test
     @DisplayName("구분자 지정 후 숫자가 입력되지 않으면 예외 발생")
     void 지정후_입력안됨_예외() {
-        Assertions.assertThrows(
-                InvalidInputException.class,
-                () -> DelimiterExtractor.extract("//;\n"), ErrorMessage.NO_NUMBER_AFTER_CUSTOM
-        );
+        assertThatThrownBy(() -> DelimiterExtractor.extract("//;\n"))
+                .isInstanceOf(InvalidInputException.class)
+                .hasMessage(ErrorMessage.NO_NUMBER_AFTER_CUSTOM);
     }
 
     @Test
     @DisplayName("커스텀 구분자가 2글자 이상 입력되면 예외 발생")
     void 커스텀구분자_두글자_이상_예외() {
-        Assertions.assertThrows(
-                InvalidInputException.class,
-                () -> DelimiterExtractor.extract("//:'\n2:3'5"), ErrorMessage.MULTIPLE_CHAR_DELIMITER
-        );
+        assertThatThrownBy(() -> DelimiterExtractor.extract("//:'\n2:3'5"))
+                .isInstanceOf(InvalidInputException.class)
+                .hasMessage(ErrorMessage.MULTIPLE_CHAR_DELIMITER);
     }
 }
